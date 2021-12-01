6ed0d1a28c001d804eec758a07cf28c8052d8b0f
Hsiu Ping Gay <hsiups@192.168.1.34>
2021-12-01 20:57:17 +0700

    updated express database configs

--- routes.js
+++ routes.js
@@ -3,15 +3,31 @@ import cookieParser from 'cookie-parser';
 import jsSHA from 'jssha';
 import { DateTime } from 'luxon';
 import { render } from 'ejs';
+// ...
 
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+} else {
+  // this is the same value as before
+  pgConnectionConfigs = {
+    user: 'hsiups',
+    host: 'localhost',
+    database: 'scheduler',
+    port: 5432,
+  };
+}
 const { Pool } = pg;
-const pgConnectionConfigs = {
-  user: 'hsiups',
-  host: 'localhost',
-  database: 'scheduler',
-  port: 5432,
-};
 const pool = new Pool(pgConnectionConfigs);
+
 export const SALT = 'aiho aiho off to the mines we go';
 
 // >>>>> po related <<<<< //
